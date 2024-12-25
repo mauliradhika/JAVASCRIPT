15b226b810e4abb50077339cf49eadf15d2f7c5d
Mauli Radhika <mauliradhika05@gmail.com>
2024-12-25 20:53:10 +0530

    25-12-2024

---
+++ Basics/numbers_maths.js
@@ -0,0 +1,16 @@
+const score = 400;
+
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(typeof(balance));
+console.log(balance.toFixed(2));
+
+const number = 28.8966;
+console.log(number.toPrecision(4));//returns a string value
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString('en-IN'));
