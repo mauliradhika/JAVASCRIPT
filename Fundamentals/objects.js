55129fd748dd114ad1595da21970bbf9d1048907
Mauli Radhika <mauliradhika05@gmail.com>
2024-12-27 02:06:51 +0530

    27-12-2024

--- Fundamentals/objects.js
+++ Fundamentals/objects.js
@@ -1,8 +1,36 @@
-//Singleton :if made through constructor - (Object.create)
+//Singleton:if made through constructor - (Object.create)
 //NOT Singleton: if made through literals
 
 //Object Literals
-const JsUser = {
-    name : "Mauli"
 
+const mySym = Symbol("key1");
+
+const JsUser = {
+    name: "Mauli",
+    mySym: "mykey1",//[mySym] -- this refers to a symbol (&not string)
+    age: 18,
+    location: "Nagpur",
+    email: "mauli@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"],
+    "Full Name": "Mauli Radhika",
 }
+
+console.log(JsUser.isLoggedIn);
+console.log(JsUser["isLoggedIn"]);//stored as a String
+console.log(JsUser["Full Name"]);
+console.log(JsUser.mySym);//to access symbol -- JsUser[mySym], no "_" req
+
+console.log(typeof JsUser.mySym);//string, but we wante dthe output to be symbol
+
+JsUser.email = "mauli.r@gmail.com";
+//Object.freeze(JsUSer);//you can't manipulate the values after this command
+
+JsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
