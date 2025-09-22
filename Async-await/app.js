7eb141f29475f89b50928dbc45ef8da355374be6
Mauli Radhika <mauliradhika05@gmail.com>
2025-09-22 23:03:17 +0530

    Async-await

--- Async-await/app.js
+++ Async-await/app.js
@@ -1,7 +1,51 @@
-console.log("one")
-console.log("two")
-setTimeout( () => {
-    console.log("Hello!");
-} , 2000) //timeout
-console.log("three")
-console.log("four")
+//async function always returns a promise
+
+async function hello(){
+    console.log("Hello!!")
+}
+
+//await pauses the execution of its surrounding async function until the promise is settled
+
+function api(){
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            console.log("weather data")
+            resolve(200)
+        }, 2000)
+    })
+}
+
+async function getWeatherData(){
+    await api();//1st 
+    await api();//2nd
+}
+
+function getData(dataId, getNextData){
+    return new Promise((resolve, reject) => {
+        setTimeout( () => { 
+        console.log("data", dataId)
+        resolve("Success") 
+        //reject("Error")
+        if(getNextData){
+            getNextData();
+        }
+        } , 5000)
+    });
+}
+
+async function getAllData() {
+    console.log("getting data1..")
+    await getData(1);
+    console.log("getting data1..")
+    await getData(2);
+    console.log("getting data1..")
+    await getData(3);
+}
+
+//IIFE: Immediately Invoked Function Expression
+// (function (){
+//
+//  })();
+//normal function, async, arrow function -> but w/o name
+//semi-colon in IIFE is important.
+
